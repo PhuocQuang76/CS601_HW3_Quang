a0fd09c5b59589b8bc040f9314667ab87fdca19a
phuocquang76 <quanghongphuoc@yahoo.com>
2019-05-31 19:22:05 -0700

    javascrtipt not working, please fix

--- js/calculation.js
+++ js/calculation.js
@@ -1,6 +1,11 @@
 alert("WELCOME TO THE WEBPAGE");
-const insert = () => {
+const calculate = () => {
     let response = 'y';
+
+    let number1;
+    let number2;
+    let total;
+
     let visitorName = prompt("Please enter your name:","type your name here");
     alert(visitorName + ", welcome to addition calculation.");
 
@@ -11,11 +16,18 @@ const insert = () => {
             firstNumber = prompt("PLease enter your first number:").trim();
         }
 
-        let secondNumber = parseInt(prompt("PLease enter your second number:"));
-        while(secondNumber.length == 0 || isNaN(secondNumber)){
-            alert("Invalid input. Please input a numnber.");
-            secondNumber = prompt("PLease enter your first number:").trim();
-        }
+        appendtape(firstNumber);
+        appendtape("+");
+        let secondNumber = "";
+        setTimeout(()=>{
+            secondNumber = parseInt(prompt("PLease enter your second number:"));
+            while(secondNumber.length == 0 || isNaN(secondNumber)){
+                alert("Invalid input. Please input a numnber.");
+                secondNumber = prompt("PLease enter your first number:").trim();
+            }
+        }, 500);
+
+        appendtape(secondNumber);
 
         let total = parseInt(firstNumber) + parseInt(secondNumber);
         alert("The sum of your two numbers is: " + total);
@@ -27,9 +39,20 @@ const insert = () => {
         }
         response = prompt("Do you want to try the calculation again? (y = \"yes\", n = \"no\")");
     }while(response == 'y' || response == 'Y' );
-    document.attachEvent("onreadystatechange", () => {
-        if (document.readyState === "complete" )
-            alert("Thank you for using my calculation program.");
-    });
+};
 
-}
+const appendtape = (number) => {
+    var node = document.createElement("div");
+    var textnode = document.createTextNode(number);
+    node.appendChild(textnode);
+    document.getElementById("tape").appendChild(node);
+};
+
+const cleartape = () => {
+    document.getElementById("tape").innerHTML = "";
+};
+
+const greetings = () => {
+    if (document.readyState === "complete" )
+        alert("Thank you for using my calculation program.");
+};
